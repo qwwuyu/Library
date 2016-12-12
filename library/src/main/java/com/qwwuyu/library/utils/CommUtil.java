6550039b61570dafa380189f44701e23793f9472
wei.qi <wei.qi@yg-ai.com>
2016-12-12 17:30:29 +0800

    删除1.0版本

--- library/src/main/java/com/qwwuyu/library/utils/CommUtil.java
+++ library/src/main/java/com/qwwuyu/library/utils/CommUtil.java
@@ -1,5 +1,7 @@
 package com.qwwuyu.library.utils;
 
+import android.content.pm.PackageManager;
+
 /**
  * 通用的工具类
  * Created by qiwei on 2016/12/1
@@ -7,5 +9,14 @@ package com.qwwuyu.library.utils;
 public class CommUtil {
     public static boolean isEmpty(CharSequence str) {
         return str == null || str.length() == 0;
+    }
+
+    public static boolean isPackageInstalled(String packageName, PackageManager packageManager) {
+        try {
+            packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);//GET_UNINSTALLED_PACKAGES
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 }
