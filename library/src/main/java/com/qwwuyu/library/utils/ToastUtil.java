0f1909f950720e5376d5e89cabf80e08725511da
wei.qi <wei.qi@yg-ai.com>
2016-12-12 19:43:16 +0800

    优化ToastUtil 简化gradle

--- library/src/main/java/com/qwwuyu/library/utils/ToastUtil.java
+++ library/src/main/java/com/qwwuyu/library/utils/ToastUtil.java
@@ -10,44 +10,67 @@ import android.widget.Toast;
  * Created by qiwei on 2016/12/1
  */
 public class ToastUtil {
-    /**  */
+    /** 主线程Handler */
     private static Handler handler = new Handler(Looper.getMainLooper());
+    /** 主线程id */
+    private static long threadID = Looper.getMainLooper().getThread().getId();
+    /** 懒加载单例Toast */
     private static Toast toast;
+    /** ApplicationContext */
+    private static Context appContext;
 
     private ToastUtil() {
         throw new UnsupportedOperationException("can't instantiate");
     }
 
-    /** 显示Toast */
-    public static void showToast(final Context context, final Object text) {
-        if (text == null || context == null) return;
-        showToast(context, text.toString(), Toast.LENGTH_SHORT);
+    public synchronized static void init(Context context) {
+        if (appContext == null) {
+            appContext = context.getApplicationContext();
+        }
     }
 
     /** 显示Toast */
-    public static void showToast(final Context context, final int id) {
-        if (context == null) return;
-        showToast(context, context.getResources().getText(id));
+    public static void showToast(final Object text) {
+        check();
+        if (text == null) return;
+        showToast(text.toString(), Toast.LENGTH_SHORT);
     }
 
-    /** 显示Toast */
-    private static void showToast(final Context context, final CharSequence text, final int duration) {
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                if (toast == null) {
-                    toast = makeText(context.getApplicationContext(), text, duration);
-                } else {
-                    toast.setText(text);
-                    toast.setDuration(duration);
+    /** 根据StringID显示Toast */
+    public static void showToast(final int id) {
+        check();
+        showToast(appContext.getResources().getText(id), Toast.LENGTH_SHORT);
+    }
+
+    private static void showToast(final CharSequence text, final int duration) {
+        if (threadID == Thread.currentThread().getId()) {
+            show(text, duration);
+        } else {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    show(text, duration);
                 }
-                toast.show();
-            }
-        });
+            });
+        }
+    }
+
+    private static void show(final CharSequence text, final int duration) {
+        if (toast == null) {
+            toast = makeText(appContext, text, duration);
+        } else {
+            toast.setText(text);
+            toast.setDuration(duration);
+        }
+        toast.show();
     }
 
     /** 创建一个Toast */
     private static Toast makeText(Context context, CharSequence text, int duration) {
         return Toast.makeText(context, text, duration);
+    }
+
+    private static void check() {
+        if (appContext == null) throw new IllegalStateException("ToastUtil must be init before using");
     }
 }
